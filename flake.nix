d821c0c79de32fb8a4f48cb9bc7fd5e712c546bb
daviaaze <22331100+daviaaze@users.noreply.github.com>
2026-05-11 06:39:58 +0000

    chore: Update Zen Browser to 1.19.12b

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
 
-    version = "1.19.11b";
+    version = "1.19.12b";
     src = pkgs.fetchurl {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256-Zd8//rMfgEaspVrXDWzwxRMMJbHo5HCgR+VzK5gTfxk=";
+      sha256 = "sha256-FW+2jDpRZFUU/dX89zTTVsmqbJgBOVr3XnIxEWKgA3k=";
     };
 
     runtimeLibs = with pkgs;
