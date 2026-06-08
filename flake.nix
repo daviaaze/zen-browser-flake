f778b7f3715ab13dd30fc581687385503967c33c
Davi Alves de Azevedo <daviaaze@gmail.com>
2026-06-08 09:44:23 -0300

    chore: Update Zen Browser to 1.20.2b (#27)
    Co-authored-by: daviaaze <22331100+daviaaze@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
 
-    version = "1.20.1b";
+    version = "1.20.2b";
     src = pkgs.fetchurl {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256-Q/qM3gLWC0cxZP176O5vwKnw3AcSiPBFaFDsVuV1wJo=";
+      sha256 = "sha256-Nq1ETAbCKw6h09HRUBPzK8nObqkHBwqyd/cA/eC8FnQ=";
     };
 
     runtimeLibs = with pkgs;
