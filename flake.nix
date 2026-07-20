d256ecd4d2600ef4d3bb17502b9806510f61c3e1
daviaaze <22331100+daviaaze@users.noreply.github.com>
2026-07-20 06:18:23 +0000

    chore: Update Zen Browser to 1.21.8b

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
 
-    version = "1.21.6b";
+    version = "1.21.8b";
     src = pkgs.fetchurl {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256-7/SQFSwhuaVbhjud8CNwwwVn4QJ/44ju6TntQIK1OyA=";
+      sha256 = "sha256-BaNmV+TJ2+cA5UouOUH3r0ZWq74zBpCqdNjPeTrzlvQ=";
     };
 
     zen-unwrapped = pkgs.stdenv.mkDerivation {
