81e0e3e456e500abf9eb8f954a654c6e227da570
Davi Alves de Azevedo <daviaaze@gmail.com>
2026-08-13 16:15:25 -0300

    Refactor Firefox wrapper configuration in flake.nix

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
   in {
     packages.${system} = {
       inherit zen-unwrapped;
-      default = (pkgs.wrapFirefox zen-unwrapped {
+      default = pkgs.wrapFirefox zen-unwrapped {
         pname = "zen-browser";
         applicationName = "zen";
         libName = "zen-${version}";
@@ -84,27 +84,7 @@
         extraPolicies = {
           DisableAppUpdate = true;
         };
-      }).overrideAttrs (old: {
-        # Remove the three-element group ["--set-default" "MOZ_ENABLE_WAYLAND" "1"]
-        # that the firefox wrapper injects. A flat list.filter would also drop the
-        # standalone "1" values from --set MOZ_LEGACY_PROFILES/MOZ_ALLOW_DOWNGRADE.
-        makeWrapperArgs = let
-          args = old.makeWrapperArgs or [];
-          go = acc: rest:
-            if rest == [] then acc
-            else
-              let
-                flag = builtins.elemAt rest 0;
-                key = builtins.elemAt rest 1;
-                val = builtins.elemAt rest 2;
-              in
-                if flag == "--set-default" && key == "MOZ_ENABLE_WAYLAND" && val == "1" then
-                  # skip the triplet
-                  go acc (builtins.drop 3 rest)
-                else
-                  go (acc ++ [flag]) (builtins.drop 1 rest);
-        in go [] args;
-      });
+      };
     };
   };
 }
